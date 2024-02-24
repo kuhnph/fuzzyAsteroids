35b8cf068d14d754340be935e65f009dbc0ad2f4
kuhnph <60302149+kuhnph@users.noreply.github.com>
2024-02-24 15:54:42 -0500

    Update game.py. Hopefully this works now

--- Game/game.py
+++ Game/game.py
@@ -1,12 +1,11 @@
 import pygame
 from models import Spaceship, Asteroid, Bullet, Target, Peach
 from utils import load_sprite, wrap_position, get_random_position
-from pygame.math import Vector2            # Importing the Vector2 class for 2D vectors
 
 class SpaceRocks:
-    MIN_ASTEROID_DISTANCE = 150
-    SCREEN_WIDTH = 1000
-    SCREEN_HEIGHT = 600
+    MIN_ASTEROID_DISTANCE = 250
+    SCREEN_WIDTH = 1920
+    SCREEN_HEIGHT = 1080
     def __init__(self):
         self._init_pygame()
         # self.screen = pygame.display.set_mode((800, 600))
@@ -16,14 +15,14 @@ class SpaceRocks:
 
         self.asteroids = []
         self.bullets = []
-        self.spaceship = Spaceship((200, self.SCREEN_HEIGHT/2), self.bullets.append)
+        self.spaceship = Spaceship((400, 300), self.bullets.append)
         # Added Peach
         self.peach = Peach((300, 400))
 
-        self.capture_agents = [self.spaceship]
-        self.target = (get_random_position(self.screen), 10.0)
+        self.capture_agents = [self.peach]
+        self.target = Target(get_random_position(self.screen), 10.0)
 
-        for _ in range(3):
+        for _ in range(1):
             while True:
                 position = get_random_position(self.screen)
                 # If asteroid hits spaceship or peach, break
@@ -36,7 +35,7 @@ class SpaceRocks:
                 ):
                     break
 
-            self.asteroids.append(Asteroid(position, self.asteroids.append, moving=True))
+            self.asteroids.append(Asteroid(position, self.asteroids.append))
 
     def main_loop(self):
         while True:
