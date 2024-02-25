8f79b8e5d57576d888d4a8f9cb640f5a0cf83bae
kuhnph <60302149+kuhnph@users.noreply.github.com>
2024-02-24 23:22:40 -0500

    Update __main__.py
    Main now runs the play step in a loop. Adds input to spacerocks for user inputs

--- Game/__main__.py
+++ Game/__main__.py
@@ -1,5 +1,6 @@
 from game import SpaceRocks
 
-if __name__ == "__main__":
-    space_rocks = SpaceRocks()
-    space_rocks.main_loop()
+if __name__ == "__main__":    
+    space_rocks = SpaceRocks(user_input=True)
+    while True:
+        space_rocks.play_step()
